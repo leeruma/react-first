67ac36a41b0b64496d6e1ebe5128ef2a6379157d
leeruma <achieve4525@naver.com>
2023-09-25 14:46:16 +0900

    전개연산자로 배열값 완전복사

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,14 @@ import './style.scss';
 // 결국 같은 값을 가리키고 있는 두 개의 링크만 복사 됨
 // 복사가된 링크의 값을 바꾸면 결국 원본값이 회손됨. (shallow copy) 불변성 유지 안됨
 
+// 리액트 개발시 불변성이 중요한 이유
+// 리액트는 원본이 있어야 복사본을 통해서 차이점을 비교분석
+// 리액트 안에서 배열이나, 객체같은 참조형 자료는 무조건 deep copy를 해서 데이터를 변경해야됨
+
 function App() {
 	let arr = ['reading', 'game', 'cook'];
-	let newArr = arr;
+	// 전개연산자 (Spread Operator) : heap메모리에 있는 값을 물리적으로 꺼내서 전개
+	let newArr = [...arr];
 	newArr[0] = 'exercise';
 	console.log(newArr);
 	console.log(arr);
