c01bbe4f90556a2fdcecad2ae14100cc49d55d69
leeruma <achieve4525@naver.com>
2023-09-25 15:39:33 +0900

    useState를 통한 state값 변경으로 화면 재 랜더링

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,21 @@
-import Footer from './components/Footer';
-import Header from './components/Header';
-import List from './components/List';
-import Popup from './components/Popup';
 import './style.scss';
 
-// 원시형자료 : 메모리, 값 자체가 callstack에서 생성된 다음에 저장
-// 원시형자료는 변수값을 다른 변수에 복사했을 때 값 자체가 복사되는 deep copy;
-// 복사된 값을 변경해도 원본은 그대로 유지되는 불변성 유지(immutable)
-
-// 참조형자료 : 메모리(callstack), 배열, 객체등의 값 자체는 (heap memory) 생성됨
-// callstack에 있는 메모리에는 배열의 값 자체가 담기는게 아닌 힙 메모리에 있는 값의 위치값이 담김
-// 참조링크가 담겨있는 변수를 새로운 변수에 옮겨담으면 값이 복사되는 것이 아닌 참조링크만 복사됨
-// 결국 같은 값을 가리키고 있는 두 개의 링크만 복사 됨
-// 복사가된 링크의 값을 바꾸면 결국 원본값이 회손됨. (shallow copy) 불변성 유지 안됨
-
-// 리액트 개발시 불변성이 중요한 이유
-// 리액트는 원본이 있어야 복사본을 통해서 차이점을 비교분석
-// 리액트 안에서 배열이나, 객체같은 참조형 자료는 무조건 deep copy를 해서 데이터를 변경해야됨
+import { useState } from 'react';
 
 function App() {
-	let student = {
-		name: 'David',
-		age: 20,
-	};
+	console.log('app');
+	// const [상태값, 상태변경전용함수] = useState(초기값);
+	// 리액트 컴포넌트는 State값이 State변경함수로 변경되야지만 컴포넌트가 재랜더링됨
+	// 숫자를 증가, 감소 시킬때 전위증감연산자를 써야지만 해당 렌더링 사이클에서 바로 값이 변경되면서 다음번 랜더링에 반영됨
+	let [Num, setNum] = useState(0);
+	console.log(Num);
 
-	let newStudent = { ...student };
-	newStudent.name = 'Andy';
-	console.log(newStudent);
-	console.log(student);
-
-	let isPop = true;
-	let isFooter = false;
 	return (
 		<>
-			<Header />
-			{isFooter && <Footer />}
-			{isPop ? <Popup /> : null}
-			<List />
+			<h1>{Num}</h1>
+
+			<button onClick={() => setNum(--Num)}>minus</button>
+			<button onClick={() => setNum(++Num)}>plus</button>
 		</>
 	);
 }
@@ -55,6 +34,7 @@ export default App;
 	리액트의 성능관리를 위한 hook
 	리액트에서의 memoization - 메모리점유율을 늘려서 성능을 개선
 	자바스크립트는 기본적으로 Garbage collector에 의해서 메모리가 관리됨
+	아래의 hook을 통해서 특정 값을 강제 메모이제이션 처리하면 가비지컬렉터에서 제외함
 	memo (컴포넌트 자체를 메모이제이션)
 	useCallback (컴포넌트 안쪽의 핸들러 함수 자체를 메모이제이션)
 	useMemo (특정 핸들러함수의 리턴값을 메모이제이션)
