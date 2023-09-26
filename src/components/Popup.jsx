8315fde5e788136c9eb30a7ffd3fb31c53931342
leeruma <achieve4525@naver.com>
2023-09-26 09:45:57 +0900

    popup 안 숫자 증가

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,5 +1,24 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 function Popup() {
-	return <aside>Popup</aside>;
+	const [Num, setNum] = useState(0);
+	// 컴포넌트가 mount시 한번만 실행
+	useEffect(() => {
+		console.log('popup 생성');
+
+		return () => {
+			// 컴포넌트가 unmount시 한번만 실행
+			console.log('popup 소멸');
+		};
+	}, []);
+	return (
+		<aside>
+			<h1>{Num}</h1>
+			<button onClick={() => setNum(Num - 1)}>minus</button>
+			<button onClick={() => setNum(Num + 1)}>plus</button>
+		</aside>
+	);
 }
 
 export default Popup;
